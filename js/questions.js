1dc457bce107d4af62621d29cf3371e13b9c7786
plucafo <plucaf87@gmail.com>
2024-01-11 13:41:01 -0700

    added questions.js file to create a set of questions

---
+++ js/questions.js
@@ -0,0 +1,10 @@
+var questionSet = [
+    {
+        question: "What does HTML stand for?",
+        answers: ["A) Hyper Text Markup Language","B) Hydro Temp Market Legend","C) Hyphen Text Markup Lesson","D) Hyper Touch Module Learning"],
+        correct: "A"
+    }
+]
+
+console.log("test");
+console.log(questionSet[0].question);
