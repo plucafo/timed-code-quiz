e1797ba232918b49517050cf6c4cfa39b4d7fe1a
plucafo <plucaf87@gmail.com>
2024-01-11 10:08:54 -0700

    added and styled start button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("something");
+var initialTime = 3;
+
+function timer() {
+  var setTime = setInterval(function () {
+    document.getElementById("timerEl").innerHTML = initialTime;
+    initialTime--;
+    if (initialTime < 0) {
+      clearInterval(setTime);
+    }
+  }, 1000);
+}
+
+timer();
